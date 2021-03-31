531b12f0392a71aa5506beb4998377b4f6210a95
Kenneth <kenneth.youthmail@gmail.com>
2021-03-31 17:21:24 +0800

    Heroku testing

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.REACT_APP_SECRET_KEY);
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const publicPath = path.join(__dirname, '/public');
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -37,13 +36,9 @@ app.post("/payment", cors(), async (req, res) => {
   }
 });
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(publicPath, 'index.html'));
-// });
-
-app.get("/", (req, res) => {
-  res.send("Hello world");
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+ '/client/build/index.html'));
+});
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("Server is listening on port 4000");
